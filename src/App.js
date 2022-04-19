e9b513e4989f0886b7b1a7d7145ed1f3cb479707
Kenyon Blue <kblue1@costar.com>
2022-04-18 21:26:36 -0600

    add context provider to application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppContainer from './components/container';
 import Header from './components/header';
 import ShoppingList from './components/shopping-list';
+import { ListContext } from './context/list-context';
+
+
 
 function App() {
+  const [ listOfItems, setListofItems ] = useState([])
   return (
     <>
       <Header />
+      <ListContext.Provider value={{listOfItems, setListofItems}}>
       <AppContainer>
       <ShoppingList />
       </AppContainer>
+      </ListContext.Provider>
     </>
   );
 }
--- src/components/shopping-list.js
+++ src/components/shopping-list.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ListContext } from '../context/list-context'
+
 import EmptyList from './empty-list'
 
 const ShoppingList = () => {
+    const {listOfItems, setListofItems} = useContext(ListContext)
+
+
   return (
     <div className='w-full flex items-center justify-center h-screen '>
-            <EmptyList/>
+            {listOfItems.length <= 1 ? <EmptyList/>  : 
+            <div className=""> shopping list </div>
+            }
     </div>
   )
 }
