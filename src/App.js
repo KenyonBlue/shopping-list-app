9a80d3c57ae44d62eedf062ed0fb761ecebdc78a
Kenyon Blue <kblue1@costar.com>
2022-04-18 20:20:15 -0600

    list empty view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AppContainer from './components/container';
 import Header from './components/header';
+import ShoppingList from './components/shopping-list';
 
 function App() {
   return (
     <>
       <Header />
       <AppContainer>
+        <ShoppingList />
       </AppContainer>
     </>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
         <>
-        <div className="w-full bg-BLUE h-16 px-8 flex items-center ">
-            <p className='text-WHITE font-semibold'> SHOPPING LIST </p>
+        <div className="w-full bg-BLUE h-16 px-8 flex items-center">
+            <p className='text-WHITE font-dosis font-semibold'> SHOPPING LIST </p>
         </div>
         </>
   )
